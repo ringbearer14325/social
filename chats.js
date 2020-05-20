4ca1929aa988e5e9a607f86996d92b207f5556fc
Daniel <danielramos1981@gmail.com>
2020-05-20 17:23:43 -0400

    commit

--- chats.js
+++ chats.js
@@ -5,7 +5,27 @@ import React, { Component } from "react";
 
 
 export default class Chat extends Component{
+    super(props);
+    constructor() {
+       this: objectHash;
 
+
+    }
+
+    
+    componentDidmount() {
+
+        
+    }
+
+
+
+
+    render() {
+        return {
+
+        }
+    }
 
 
 
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import React, { Component } from "react";
 export default class Index extends Component {
 
 
+    
+
 
 
 }
