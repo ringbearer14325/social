dbaef5c1c38b394aa08b5c7197a085960329f18c
Daniel <danielramos1981@gmail.com>
2020-05-20 21:39:55 -0400

    Comiit all files. Added stylesheets/style.css, header.js and login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,14 @@
-import heruko from "heruko";
 import React, { Component } from "react";
 
 
 
 export default class Login extends Component {
+    super(props);
+    constructor() {
+        this.username = username,
+        this.password = password;
+    }
+
 
 
 
