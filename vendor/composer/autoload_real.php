dc277351d2f7e60f8a8d4930eef103981afdb2b3
Daniel <danielramos1981@gmail.com>
2020-05-20 21:12:36 -0400

    commit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -50,6 +50,24 @@ class ComposerAutoloaderInit0b3bc91cd12d054dcd3b1521094c4cb3
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit0b3bc91cd12d054dcd3b1521094c4cb3::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire0b3bc91cd12d054dcd3b1521094c4cb3($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire0b3bc91cd12d054dcd3b1521094c4cb3($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
