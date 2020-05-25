ed90cb1d3f000604bd0287c419b49cca5ebd0f0d
Daniel <danielramos1981@gmail.com>
2020-05-25 15:32:31 -0400

    commit

---
+++ firebaseconfig.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: "ADD-YOUR-DETAILS-HERE",
+  authDomain: "ADD-YOUR-DETAILS-HERE",
+  databaseURL: "ADD-YOUR-DETAILS-HERE"
+};
+firebase.initializeApp(config);
+export const auth = firebase.auth;
+export const db = firebase.database();
