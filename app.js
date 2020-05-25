6c352a4f03c354edcbfafb22675359b65854c3e4
Daniel <danielramos1981@gmail.com>
2020-05-25 19:10:18 -0400

    Commit files. Added app.js file

--- app.js
+++ app.js
@@ -1,53 +1,106 @@
 import React, { Component } from 'react';
 import {
-    Route,
-    BrowserRouter as Router,
-    Switch,
-    Redirect,
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect
 } from "react-router-dom";
-import Home from './pages/Home';
-import Chat from './pages/Chat';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import { auth } from './services/firebase';
+import Home from "./pages/home";
+import Chat from "./pages/chat";
+import Signup from "./pages/signup";
+import Login from "./pages/login";
+import './css/style.css';
 
 
 
-export default class App extends Component {
+function PrivateRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authenticated === true ? (
+          <Component {...props} />
+        ) : (
+            <Redirect
+              to={{ pathname: "/login", state: { from: props.location } }}
+            />
+          )
+      }
+    />
+  );
+}
 
-    function PrivateRoute({ component: Component, authenticated, ...rest}) {
-            return (<Route 
-                {...rest }
-                render = {(props) => authenticated === true
-                    ? <Component{...props }/>
-                     : < Redirect to = {{ pathname: '/login', state: {from: props.location } }} />}
-                      />
-                )
-            }
+function PublicRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authenticated === false ? (
+          <Component {...props} />
+        ) : (
+            <Redirect to="/chat" />
+          )
+      }
+    />
+  );
+}
+
+class App extends Component {
+  constructor() {
+    super();   
+   
+    this.state = {
+      authenticated: false,
+      loading: true,
+    };
+  }
 
 
-    function PublicRoute({ component: Compoent, authenticated, ...rest }) {
-                return(
-                    <Route
-                    {...rest}
-                    render={(props) => authenticated === false
-                        ? <Component{ ...props} />
-                        : <Redirect to='/chat' />}
-                        />
-                )
-            }
+   componentDidMount() {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          loading: false
+        });
+      } else {
+        this.setState({
+         authenticated: false,
+          loading: false
+        });
+      }
+    };
+   
+  
 
-     
-     
+  render() {
+    return this.state.loading === true ? (
+      <div className="spinner-border text-success" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+    ) : (
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <PrivateRoute
+              path="/chat"
+              authenticated={this.state.authenticated}
+              component={Chat}
+            />
+            <PublicRoute
+              path="/signup"
+              authenticated={this.state.authenticated}
+              component={Signup}
+            />
+            <PublicRoute
+              path="/login"
+              authenticated={this.state.authenticated}
+              component={Login}
+            />
+          </Switch>
+        </Router>
+      );
+  }
+}
 
 
- 
-            
-
-            private route
-
-
-            public route 
-
-            public route 
-
+export default App;
