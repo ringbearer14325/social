e5f1915605162145067388341b0358cbfec529a1
Daniel <danielramos1981@gmail.com>
2020-05-25 19:39:29 -0400

    commit all

--- firebaseconfig.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  apiKey: "ADD-YOUR-DETAILS-HERE",
-  authDomain: "ADD-YOUR-DETAILS-HERE",
-  databaseURL: "ADD-YOUR-DETAILS-HERE"
-};
-firebase.initializeApp(config);
-export const auth = firebase.auth;
-export const db = firebase.database();
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,1 +1,16 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import { signup } from '../helpers.auth';
+
+export default class signup extends Component {
+     
+   
+  render() {
+    return (
+      <div>
+          <form onSubmit={this.handleSubmit}>
+              <h1>
+                  sign up to 
+                  <link to ="/">Social</link>
+              </h1>
+          </form>
