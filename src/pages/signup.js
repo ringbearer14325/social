310a7044c098b0dd2140530cbfa9d620ac341448
Daniel <danielramos1981@gmail.com>
2020-05-27 21:39:27 -0400

    Commit files. added signup.js.

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -3,7 +3,33 @@ import { Link } from 'react-router-dom';
 import { signup } from '../helpers.auth';
 
 export default class signup extends Component {
-     
+     constructor(props) {
+     super(props);
+          this.state = {
+          error: null,
+          email: '',
+          password: '',
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+
+      handleChange(event) {
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+      }
+
+      async handleSubmit(event) {
+        event.preventDefault();
+        this.setState({ error: ''});
+        try {
+          await signup(this.state.email), this.state.password);
+        } catch (error) {
+          this.setState({ error: error.message });
+        }
+      }
    
   render() {
     return (
@@ -13,4 +39,21 @@ export default class signup extends Component {
                   sign up to 
                   <link to ="/">Social</link>
               </h1>
+              <p>Fill in the form below to create an account</p>
+              <div>
+                <input placeholder="Email" name="email" type="email" onChange={this.handleChange} value={this.state.email}></input>
+              </div>
+              <div>
+                <input placeholder="Password" name="password" onChange={this.handleChange} value={this.state.password} type="password"></input>
+              </div>
+              <div>
+                {this.state.error ? <p>{this.state.error}</p> : null}
+                <button type="submit">Sign up</button>
+              </div>
+              <hr></hr>
+              <p>Already have an account? <link to="/login">Login</link></p>
           </form>
+      </div>
+    )
+  }
+}
