0ace7505b0a21c2b922ffc6060b16dff75b8b143
Daniel <danielramos1981@gmail.com>
2020-06-10 19:14:50 -0400

    commit

---
+++ src/Cloudinary/cloudinary.js
@@ -0,0 +1,6 @@
+\Cloudinary::config(array( 
+    "cloud_name" => "nami-connect", 
+    "api_key" => "481343716387372", 
+    "api_secret" => "5ECJZ8HvwS8hbkAcgpQ0rPCNI1A", 
+    "secure" => true
+  ));
